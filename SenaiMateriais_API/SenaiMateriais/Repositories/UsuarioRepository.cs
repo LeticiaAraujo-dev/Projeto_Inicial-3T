265eb2e2872bd07432a49bc9d8f8efae842de0e7
FIC <FIC@info.local>
2021-08-05 10:40:51 -0300

    Adicionado Interface,Repositorio do login

---
+++ SenaiMateriais_API/SenaiMateriais/Repositories/UsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using SenaiMateriais.Contexts;
+using SenaiMateriais.Domains;
+using SenaiMateriais.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiMateriais.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        SMContext ctx = new SMContext();
+        public Usuario login(string email, string senha)
+        {
+
+            return ctx.Usuarios.FirstOrDefault(o => o.Email == email && o.Senha == senha);
+
+        }
+    }
+}
